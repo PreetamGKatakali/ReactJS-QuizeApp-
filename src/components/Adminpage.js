76d3adc44b18c6fc6a96e38dbb0c6085334e5c9b
PreetamGKatakali <preetam19cs051@gmail.com>
2023-04-12 22:52:29 +0530

    added

--- src/components/Adminpage.js
+++ src/components/Adminpage.js
@@ -2,7 +2,20 @@ import React from "react";
 
 function Adminpage(){
     return(
-        <div>hello</div>
+        <div>
+            <h2 className="heading-title">Hello Admin</h2>
+            <from>
+                <label className="title-heading">
+                    Enter email :
+                    <input type="text" name="email" className="name-feild"></input><br></br>
+                </label>
+                <label className="title-heading">
+                    Password :
+                    <input type="password" name="password" className="name-feild"></input>
+                </label>
+                <input type="submit" value="adminsubmit" className="submit-btn"></input>
+            </from>
+        </div>
     )
 }
 export default Adminpage
